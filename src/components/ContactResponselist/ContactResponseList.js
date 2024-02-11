2dd38371c79c1068fdbc0a0918393f7e2d5bf4f6
aditi <aditimile29082003@gmail.com>
2024-02-12 02:50:10 +0530

    add changes

--- src/components/ContactResponselist/ContactResponseList.js
+++ src/components/ContactResponselist/ContactResponseList.js
@@ -2,13 +2,16 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from "react-bootstrap/Image";
-import {Navbar,Nav }from "react-bootstrap"
+import { Navbar, Nav } from "react-bootstrap"
 import Manas from "../../assets/Manas.png";
-import envelope from "../../assets/envelope.png"
+import envelope from "../../assets/envelope.png";
+import Pagination from 'react-bootstrap/Pagination';
+import "../ContactResponselist/ContactResponseList.css"
+
 function ContactResponseList() {
   return (
     <>
-    <Navbar expand="xl">
+      <Navbar expand="xl">
         <Container>
           <h4>Logo</h4>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -20,71 +23,87 @@ function ContactResponseList() {
             </Nav>
           </Navbar.Collapse>
           <Image src={Manas} alt="./"></Image>
-    </Container>
-    </Navbar>
-    <Container fluid className="body">
-    <Row>
-      <Col x-sm className='boxenv'>
-        <Image src={envelope} alt="./" className="envelop" />
-      </Col>
-    </Row>
-    <Row className="contact3">
-      {/* <h5>Contact Response</h5> */}
-      <Col md={3}>      
-        <Row style={{border:"1px solid grey"}}>
-          <h5>Contact Response</h5>
-          <p>Request</p>
-          <p><input type="checkbox"></input>Request</p>
-          <p><input type="checkbox"></input>Request</p>
-        </Row>
-        </Col>
-        <Col x-sm={11}style={{border:"1px solid grey",marginTop:"20px",padding:"20px"}}>
-          <Row style={{border:"1px solid grey",marginTop:"10px"}}>
-            <Col><h6>Request Contact information</h6></Col>
-            <Col>jkljkljkljkl</Col>
-            <Col>jkl</Col>
-            <Col>jkl</Col>
-          </Row>
-          <Row style={{border:"1px solid grey",marginTop:"10px"}}>
-            <Col>jkljkljkljkl</Col>
-            <Col>jkl</Col>
-            <Col>jkl</Col>
-            <Col>jkl</Col>
-          </Row>
-          <Row style={{border:"1px solid grey",marginTop:"10px"}}>
-            <Col>jkljkljkljkl</Col>
-            <Col>jkl</Col>
-            <Col>jkl</Col>
-            <Col>jkl</Col>
-          </Row>
-          <Row style={{border:"1px solid grey",marginTop:"10px"}}>
-            <Col>jkljkljkljkl</Col>
-            <Col>jkl</Col>
-            <Col>jkl</Col>
-            <Col>jkl</Col>
-          </Row>
-          <Row style={{border:"1px solid grey",marginTop:"10px"}}>
-            <Col>jkljkljkljkl</Col>
-            <Col>jkl</Col>
-            <Col>jkl</Col>
-            <Col>jkl</Col>
-          </Row>
-          <Row style={{border:"1px solid grey",marginTop:"10px"}}>
-            <Col>jkl</Col>
-            <Col>jkl</Col>
-            <Col>jkl</Col>
-            <Col>jkl</Col>
-          </Row>
-          <Row style={{border:"1px solid grey",marginTop:"10px"}}>
-            <Col>jkl</Col>
-            <Col>jkl</Col>
-            <Col>jkl</Col>
-            <Col>jkl</Col>
-          </Row>
-        </Col>
-        </Row>
         </Container>
-        </>
+      </Navbar>
+      <Container fluid className="body">
+        <Row>
+          <Col x-sm className='boxenv'>
+            <Image src={envelope} alt="./" className="envelop" />
+          </Col>
+        </Row>
+        <Row className="contact3">
+          {/* <h5>Contact Response</h5> */}
+          <Col md={3}>
+            <Row style={{ border: "1px solid grey" }}>
+              <h5>Contact Response</h5>
+              <p>Response</p>
+              <p><input type="checkbox"></input>Request</p>
+              <p><input type="checkbox"></input>Received</p>
+              <p className='setting'>Setting</p>
+              <br/><p>logout</p>
+              
+            </Row>
+          </Col>
+          <Col x-sm={11} style={{ border: "1px solid grey", marginTop: "20px", padding: "20px" }}>
+            <Row style={{ border: "1px solid grey", marginTop: "10px" }}>
+              <Col><h6>Request Contact information</h6></Col>
+              <Col>Date</Col>
+              <Col>Time</Col>
+              <Col>View</Col>
+            </Row>
+            <Row style={{ border: "1px solid grey", marginTop: "10px" }}>
+              <Col><b>Contact information </b><br />Name:Suman Saha</Col>
+              <Col><br />04 Aug,2023</Col>
+              <Col><br />4:15PM</Col>
+              <Col>i</Col>
+            </Row>
+            <Row style={{ border: "1px solid grey", marginTop: "10px" }}>
+              <Col><b>Contact information </b><br />Name:Suman Saha</Col>
+              <Col><br />04 Aug,2023</Col>
+              <Col><br />4:15PM</Col>
+              <Col>i</Col>
+            </Row>
+            <Row style={{ border: "1px solid grey", marginTop: "10px" }}>
+              <Col><b>Contact information </b><br />Name:Suman Saha</Col>
+              <Col><br />04 Aug,2023</Col>
+              <Col><br />4:15PM</Col>
+              <Col>i</Col>
+            </Row>
+            <Row style={{ border: "1px solid grey", marginTop: "10px" }}>
+              <Col><b>Contact information </b><br />Name:Suman Saha</Col>
+              <Col><br />04 Aug,2023</Col>
+              <Col><br />4:15PM</Col>
+              <Col>i</Col>
+            </Row>
+            <Row style={{ border: "1px solid grey", marginTop: "10px" }}>
+              <Col><b>Contact information </b><br />Name:Suman Saha</Col>
+              <Col><br />04 Aug,2023</Col>
+              <Col><br />4:15PM</Col>
+              <Col>i</Col>
+            </Row>
+            <Row style={{ border: "1px solid grey", marginTop: "10px" }}>
+              <Col><b>Contact information </b><br />Name:Suman Saha</Col>
+              <Col><br />04 Aug,2023</Col>
+              <Col><br />4:15PM</Col>
+              <Col>i</Col>
+            </Row>
+            <Row>
+              <Col x-sm={10}>Showing 1 to 6 260 entries</Col>
+              <Col x-sm={2}>
+                <Pagination>                 
+                  <Pagination.Item active>{1}</Pagination.Item>              
+                  <Pagination.Item>{2}</Pagination.Item>
+                  <Pagination.Item>{3}</Pagination.Item>
+                  <Pagination.Ellipsis />
+                  <Pagination.Item >{65}</Pagination.Item>  
+                  <button>Next</button>               
+                </Pagination>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 export default ContactResponseList;
